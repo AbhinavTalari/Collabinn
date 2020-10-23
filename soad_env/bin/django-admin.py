4b58b7852aeee114014eccd47b388ec357315622
Abhinav Talar <abhinavtalari9@gmail.com>
2020-10-23 08:48:47 +0530

    COnfigured for Ubuntu

--- soad_env/bin/django-admin.py
+++ soad_env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\abhinav\documents\github\soad-project\soad_env\scripts\python.exe
+#!/home/ghostwire/SOAD-Project/soad_env/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
