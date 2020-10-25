02401e63d8ba0c5d53b34684701463392d9b76e9
Abhinav Talari <49162896+AbhinavTalari@users.noreply.github.com>
2020-10-25 23:56:44 +0530

    Added Frontend Pages  and User Creation Form

---
+++ collabinn/companies/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Company
+from django.contrib.auth import authenticate, login
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=100, help_text='Enter Valid Email ! Required Field')
+    class Meta:
+        model = Company
+        fields = ('company_name','location','email', 'company_uid','date_of_est','expertise','interests', 'password1', 'password2',)
+
+class AccountAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    class Meta:
+        model = Company
+        fields = ('email', 'password')
+        def clean(self):
+            if self.is_valid():
+                email = self.cleaned_data['email']
+                password = self.cleaned_data['password']
+                if not authenticate(email=email, password=password):
+                    raise forms.ValidationError("Invalid login")
--- collabinn/companies/models.py
+++ collabinn/companies/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+# accounts.models.py
+
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Email address is a must')
+        if not username:
+            raise ValueError('User Name is a must')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user	
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+        	email=self.normalize_email(email),
+        	password=password,
+        	username=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class Company(AbstractBaseUser):
+    
+    email = models.EmailField(verbose_name='email',max_length=255,unique=True)
+    company_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    company_uid = models.CharField(max_length=30, unique=True)
+    date_of_est = models.DateField(max_length=8,null=True)
+    expertise=models.TextField(verbose_name='expertise',max_length=255,null=True)
+    interests=models.TextField(max_length=255,verbose_name='interests',null=True)
+    
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) 
+    is_superuser = models.BooleanField(default=False) 
+    #passwords are built in 
+   
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['company_uid'] 
+
+    objects = UserManager()
+
+    def __str__(self):              
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific  object wise permission?"
+       
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to access app with appname `app_label`?"
+      
+        return True
+
+
+
