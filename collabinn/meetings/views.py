0c1cadc944cf99c3219b64910cb9f7618ccdb45c
ssk-2610 <shubhamkawatgi12@gmail.com>
2020-10-26 21:50:59 +0530

    sk

--- collabinn/meetings/views.py
+++ collabinn/meetings/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def meethome(request):
-    return HttpResponse('meeting home')
+    return HttpResponse('meeting home shubham')
