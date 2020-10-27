dbb62e269d2ac7702f022c728811eeb6b5b55ee3
ssk-2610 <shubhamkawatgi12@gmail.com>
2020-10-27 21:55:41 +0530

    updated urls

--- collabinn/meetings/urls.py
+++ collabinn/meetings/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth import views as auth_views
 from meetings import views
 
 urlpatterns = [
-    path("",views.meethome,name='meetings_home'),
+    path("",views.meethome_view,name='meetings_home'),
+    path("request",views.request_companies_view,name='request_companies'),
+    path("choose",views.choose_view,name='choose-companies'),
 ]
--- collabinn/meetings/views.py
+++ collabinn/meetings/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def meethome(request):
+def meethome_view(request):
     return HttpResponse('meeting home shubham')
+
+def request_companies_view(request):
+    return HttpResponse('request to other companies')
+
+def choose_view(request):
+    return HttpResponse('accept-reject companies')
