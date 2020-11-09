7c9575785bd9925fa6f4d3466c872d97485b694c
isha1610200 <44157249+isha1610200@users.noreply.github.com>
2020-11-10 03:53:53 +0530

    added meetings_tests

--- collabinn/meetings/models.py
+++ collabinn/meetings/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 class MeetingInfo(models.Model):
-    company_A=models.CharField(max_length=50,default='')
-    company_B=models.CharField(max_length=50,default='')
-    venue=models.CharField(max_length=50,default='')
+    company_A=models.CharField(max_length=50)
+    company_B=models.CharField(max_length=50)
+    venue=models.CharField(max_length=50)
     date=models.DateField()
-    security_req=models.BooleanField(default='False')
+    security_req=models.BooleanField()
+
+    def __str__(self):              
+        return "Meeting between {} and {} at {} on {}. (Where security requirement is: {} )".format(self.company_A,self.company_B,self.venue,self.date,self.security_req)
 
 
 
--- collabinn/meetings/tests.py
+++ collabinn/meetings/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import MeetingInfo
 # Create your tests here.
+
+class MeetingInfoModelTest(TestCase):
+
+	def test_string_representation(self):
+		meeting_info = MeetingInfo(company_A = "zara",company_B = "Madame",venue = "Elante",security_req = True,date = 30/11/2020)
+		my_str = "Meeting between {} and {} at {} on {}. (Where security requirement is: {} )".format(meeting_info.company_A,meeting_info.company_B,meeting_info.venue,meeting_info.date,meeting_info.security_req)
+		self.assertEqual(str(meeting_info),my_str)
+
+
+
+class MeetingViewsTest(TestCase):
+
+	pass
+
