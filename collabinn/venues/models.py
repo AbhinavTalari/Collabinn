0aa26cf209dfb034cefff8534bbef1fa89a25e29
ssk-2610 <shubhamkawatgi12@gmail.com>
2020-11-12 12:47:14 +0530

    minor changes

--- collabinn/venues/models.py
+++ collabinn/venues/models.py
@@ -5,8 +5,8 @@ class DestinationInfo(models.Model):
     no_of_rooms=models.IntegerField(default=0)
     destination=models.CharField(max_length=50,default='')
     weather=models.CharField(max_length=50,default='')
-    no_adults=models.IntegerField(default=0)
-    no_children=models.IntegerField(default=0)
+    men=models.IntegerField(default=0)
+    women=models.IntegerField(default=0)
     checkin=models.DateField(default=datetime.date.today)
     checkout=models.DateField(default=datetime.date.today)  
 
