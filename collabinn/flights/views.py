abca5430a3e460d6d40b1066d651708bf5cc2034
Abhinav Talari <49162896+AbhinavTalari@users.noreply.github.com>
2020-12-12 19:18:17 +0530

    Flights Implemented

--- collabinn/flights/views.py
+++ collabinn/flights/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import FlightForm
+from amadeus import Client, ResponseError 
+from django.contrib import messages 
+from django.shortcuts import render
+
+amadeus = Client(client_id='pnGpZA5SGjPfciAwncPmVWFryuEVGeb1', 
+                 client_secret='Wneu30CGlAKfeZBQ', 
+                 log_level='debug')
+
+
 
 def home_view(request):
-    context={}
-    if request.POST:
-        form=FlightForm(request.POST)
-        if form.is_valid():
-            form.save()
-            sk=form.cleaned_data
-            print(sk)
-            return render(request,'flights/index.html',context)
-        else:  
-            context['form']=form  
-            return render(request,'flights/index.html',context)
-    else:
-        form = FlightForm()
-        context['form'] = form
-        return render(request,'flights/index.html',context)
+    kwargs = {'originLocationCode': request.POST.get('start'),'destinationLocationCode':request.POST.get('destination'),'departureDate':request.POST.get('dep_date'),'adults':request.POST.get('adults')} 
+    try: 
+        purpose = amadeus.shopping.flight_offers_search.get(**kwargs).data
+        print(type(purpose))
+    except ResponseError as error: 
+        print(error) 
+        messages.add_message(request, messages.ERROR, error) 
+        return render(request, 'flights/index.html', {}) 
+    return render(request, 'flights/index.html', {'prediction': purpose}) 
 
 def contact(request):
     return render(request,'flights/contact.html')
